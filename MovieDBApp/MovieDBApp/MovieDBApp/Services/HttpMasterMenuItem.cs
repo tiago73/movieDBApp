3604a36d42cd1f3ec67fa7ec8328ae62b9157e69
tiago73 <tiagodsf73@gmail.com>
2019-10-29 21:55:08 -0300

    Implementação dos modelos de classes e implementação das classes de requisição HTTP utilizando tanto Newtojson e o system.text.json

---
+++ MovieDBApp/MovieDBApp/MovieDBApp/Services/HttpMasterMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDBApp.Services
+{
+
+    public class HttpMasterMenuItem
+    {
+        public HttpMasterMenuItem()
+        {
+            TargetType = typeof(HttpMasterMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
